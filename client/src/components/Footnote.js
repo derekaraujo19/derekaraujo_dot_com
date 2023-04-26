03e3d716bac5f40c4c19fcabe79a233814570158
Derek Araujo <derekaraujo19@gmail.com>
2023-04-25 20:35:00 -0400

    some styling done, home page w/ pic implemented

--- client/src/components/Footnote.js
+++ client/src/components/Footnote.js
@@ -7,14 +7,14 @@ function Footnote(){
 const style = { color: "black", fontSize: "1.5em" }
 
   return (
-    <div className="Icons">
-      <a href="https://github.com/derekaraujo19" style={style} target="_blank" rel='noopener noreferrer'> <FaGithub /> </a>
-      <a href="https://www.linkedin.com/in/derek-araujo/" target="_blank" rel="noopener noreferrer" style={style}><FaLinkedin /> </a>
-      <a style={style} href="https://medium.com/@derekaraujo19" target="_blank" rel="noopener noreferrer"> <FaMediumM/> </a>
-      <a href="https://open.spotify.com/artist/0RW4vRpFksiQDl8gXO7NPI?si=QccPTEkFTl6JPrMEPPJm8Q" target="_blank" rel="noopener noreferrer" style={style}> <FaSpotify /></a>
-      <a href="https://www.instagram.com/derek_araujo/" style={style} target="_blank" rel='noopener noreferrer'> <FaInstagram/> </a>
-      <a href="mailto:derekaraujo19@gmail.com" style={style} target="_blank" rel='noopener noreferrer'> <FaEnvelope /> </a>
-    </div>
+        <footer className="Icons">
+          <a href="https://github.com/derekaraujo19" style={style} target="_blank" rel='noopener noreferrer'> <FaGithub /> </a>
+          <a href="https://www.linkedin.com/in/derek-araujo/" target="_blank" rel="noopener noreferrer" style={style}><FaLinkedin /> </a>
+          <a style={style} href="https://medium.com/@derekaraujo19" target="_blank" rel="noopener noreferrer"> <FaMediumM/> </a>
+          <a href="https://open.spotify.com/artist/0RW4vRpFksiQDl8gXO7NPI?si=QccPTEkFTl6JPrMEPPJm8Q" target="_blank" rel="noopener noreferrer" style={style}> <FaSpotify /></a>
+          <a href="https://www.instagram.com/derek_araujo/" style={style} target="_blank" rel='noopener noreferrer'> <FaInstagram/> </a>
+          <a href="mailto:derekaraujo19@gmail.com" style={style} target="_blank" rel='noopener noreferrer'> <FaEnvelope /> </a>
+        </footer>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,16 @@
+
 function Home() {
+
+
   return (
     <div className="Home">
-      <h3>HOME</h3>
+      <p id="homeText">IS A NY BASED software engineer & MUSIC PRODUCER. HE HAS his certificate from the flatiron school, and has BEEN RELEASING MUSIC SINCE 2014 UNDER THE ARTIST BANNER STRANGETHINGS.</p>
+      <img src="/images/strangethings-01.jpg"
+            id="homeImage"
+           alt="Derek"
+           height='600px'
+           width='auto'
+      />
     </div>
   );
 }
