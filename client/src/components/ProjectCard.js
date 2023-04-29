dc11f7dc5db0ee05a52691a481904e59edbe24e5
Derek Araujo <derekaraujo19@gmail.com>
2023-04-28 20:27:25 -0400

    api request made for projects;project card component created

---
+++ client/src/components/ProjectCard.js
@@ -0,0 +1,9 @@
+function ProjectCard({project}) {
+  return (
+    <div className="ProjectCard">
+      <h3>{project.title}</h3>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,6 +1,25 @@
+import React, { useState, useEffect } from "react";
+import ProjectCard from "./ProjectCard";
+
 function Projects(){
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/projects')
+      .then((r) => r.json())
+      .then((projects) => setProjects(projects));
+  }, []);
+
+
   return(
-    <h1>Projects!!!!</h1>
+    <div className="Projects">
+      {projects.map((project) => (
+        <ProjectCard
+          key={project.id}
+          project={project}
+        />
+      ))}
+    </div>
   );
 }
 
