7ea545763816a321ce839fba7e40bfff56416e63
Derek Araujo <derekaraujo19@gmail.com>
2023-05-09 22:48:04 -0400

    project cards with images and links implemented

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,8 +1,22 @@
+import { FaExternalLinkAlt, FaGithub, FaYoutube } from 'react-icons/fa';
+
 function ProjectCard({project}) {
+
   return (
     <div className="ProjectCard">
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
+      <img src={project.screenshot}
+        alt="Screenshot"
+        width='700px'
+        height='389'
+        style={{borderRadius:'2px'}}
+      />
+      <h2>{project.title}</h2>
+      <p id="proj_desc">{project.description}</p>
+      <div className="ProjIconList">
+        {project.deploy_link ? <li><a href={project.deploy_link} className="ProjIcons" target="_blank" rel='noopener noreferrer'> <FaExternalLinkAlt/> Deployed </a></li> : ""}
+        <li><a href={project.github_link} className="ProjIcons" target="_blank" rel='noopener noreferrer'> <FaGithub/> Github </a></li>
+        {project.demo_link ? <li><a href={project.demo_link} className="ProjIcons" target="_blank" rel='noopener noreferrer'> <FaYoutube/> Demo </a></li> : ""}
+      </div>
     </div>
   );
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,29 +14,34 @@ puts "Seeding Projects..."
 
 # Liner Notes
 liner = Project.create!(
-  screenshot: "",
+  screenshot:"/images/liner.png",
   title: "Liner Notes",
-  description: "Liner Notes is a database and note-taking app for artists to keep track of their song credits and associated albums. Users create accounts to log their artistic work, leave notes for specific songs, and add, edit, or delete any album metadata.",
+  description: "Database and note-taking app for artists to keep track of their song and album credits.",
   deploy_link: "https://liner-notes-app.onrender.com/",
   github_link: "https://github.com/derekaraujo19/liner-notes-final",
-  demo_link: "https://www.youtube.com/watch?v=W5fqM41CFrk&ab_channel=DerekAraujo", tech_used: "React, Ruby on Rails, 'bcrypt' and user authorization")
+  demo_link: "https://www.youtube.com/watch?v=W5fqM41CFrk&ab_channel=DerekAraujo",
+  tech_used: "React, Ruby on Rails, 'bcrypt' and user authorization")
 
 # BringAJacket
 jacket = Project.create!(
-  screenshot: "",
-  title: "Bring-A-Jacket")
+  screenshot: "/images/jacket.png",
+  title: "Bring-A-Jacket",
+  description: "recommends appropriate outerwear based on a user's inputted location.")
 
 # Portfolio
 portfolio = Project.create!(
-  screenshot: "",
+  screenshot: "/images/portfolio.png",
   title: "Personal Portfolio",
-  description: "This portfolio showcases my software projects and music production and songwriting credits.",
+  description: "Showcase of software projects and produced music.",
   deploy_link: "https://derek-araujo-app.onrender.com/",
   github_link: "https://github.com/derekaraujo19/derekaraujo_dot_com",
   demo_link: "",
-  tech_used: "React, Ruby on Rails")
+  tech_used: "React, Ruby on Rails"
+)
 
 # DiscSnob
 discsnob = Project.create!(
-  title: "DiscSnob"
+  screenshot: "/images/discsnob.png",
+  title: "DiscSnob",
+  description: "a social hub where users can leave hot takes on their favorite albums."
 )
