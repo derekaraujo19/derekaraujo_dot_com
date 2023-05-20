d9c0bf381ae4e8a10ba6297c9fa9f25954b06dd0
Derek Araujo <derekaraujo19@gmail.com>
2023-05-20 12:58:13 -0400

    Home page grid corrected

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,28 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 function Home() {
 
 
   return (
     <div className="Home">
-      <Row className="rows">
-        <Col className="columns" lg={{offset:1}} >
-          <p id="homeText">IS A NY BASED software engineer & MUSIC PRODUCER. HE received his certificate from flatiron school, and has BEEN RELEASING MUSIC SINCE 2014 UNDER THE ARTIST BANNER <a href="https://open.spotify.com/artist/0RW4vRpFksiQDl8gXO7NPI?si=QccPTEkFTl6JPrMEPPJm8Q" target="_blank" rel='noopener noreferrer' >STRANGETHINGS</a>.</p>
-        </Col>
-        <Col className="columns" lg={6} >
-          <img src="/images/strangethings-01.jpg"
-            id="homeImage"
-            alt="Derek"
-            style={{borderRadius:'3px'}}
-          />
-        </Col>
-      </Row>
+      <Container>
+        <Row className="rows">
+          <Col className="px-4 columns" xl={6} lg={6} md={12} >
+            <p id="homeText">IS A NY BASED software engineer & MUSIC PRODUCER. HE received his certificate from flatiron school, and has BEEN RELEASING MUSIC SINCE 2014 UNDER THE ARTIST BANNER <a href="https://open.spotify.com/artist/0RW4vRpFksiQDl8gXO7NPI?si=QccPTEkFTl6JPrMEPPJm8Q" target="_blank" rel='noopener noreferrer' >STRANGETHINGS</a>.</p>
+          </Col>
+          <Col className="px-4 columns" xl={6} lg={6} md={12} >
+            <img src="/images/strangethings-01.jpg"
+              id="homeImage"
+              className="img-fluid"
+              alt="Derek"
+              style={{borderRadius:'3px'}}
+            />
+          </Col>
+        </Row>
+      </Container>
+
 
 
     </div>
